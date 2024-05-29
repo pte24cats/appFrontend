e8fcbeb9016d6b5006ecbc751e7d7523ebe16735
Vanessa <vanessa.munson03@gmail.com>
2024-05-29 23:44:32 +0200

    Routes for home and our-cats components

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { OurCatsComponent } from './our-cats/our-cats.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent,
+        title: '[Site Title]'
+    },
+    {
+        path: 'our-cats',
+        component: OurCatsComponent,
+        title: 'Our Cats'
+    }
+
+];
