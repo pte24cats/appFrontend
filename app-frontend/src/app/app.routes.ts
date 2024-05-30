6c001f42deb085e91a7b6e95109d1d1d88a279e4
Vanessa <vanessa.munson03@gmail.com>
2024-05-30 13:38:57 +0200

    Add routes for about and sign-in

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,18 +1,30 @@
 import { Routes } from '@angular/router';
-import { OurCatsComponent } from './our-cats/our-cats.component';
 import { HomeComponent } from './home/home.component';
+import { OurCatsComponent } from './our-cats/our-cats.component';
+import { AboutComponent } from './about/about.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
 
     {
         path: '',
         component: HomeComponent,
-        title: '[Site Title]'
+        title: 'pte24cats'
     },
     {
         path: 'our-cats',
         component: OurCatsComponent,
         title: 'Our Cats'
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About Us'
+    },
+    {
+        path: 'sign-in',
+        component: SignInComponent,
+        title: 'Sign In'
     }
 
 ];
