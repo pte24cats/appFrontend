2a2a2201c8870d44b76119aadb46a176db97599a
Alaa Dardari <al.dard101@gmail.com>
2024-06-18 10:27:02 +0400

    Fixed the sign in button not working by adding missing providers and imports, but there's still a few issues:
    - The sign in opens its own page rather than doing the pop-up thing...
    - ...But when going to our-cats, then going to sign in, the popup works
    - You cannot exit the popup by any means (pressing x or clicking in the blurred area)
    - When pressing on "Adopt a cat", the Nav bar gets shifted to the left for some reason

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router'; // Import RouterModule
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import  {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -23,7 +25,10 @@ import { HomeComponent } from './home/home.component';
 
   ],
 
-  providers: [],
+  providers: [
+    NgbActiveModal,
+    NgbModule,
+  ],
 
 })
 
