a88e49c13134fb72fb6f30b6c140a3e675e4f3c3
Vanessa <vanessa.munson03@gmail.com>
2024-06-19 09:12:26 +0200

    Added RouterLink for routes

--- app-frontend/src/app/our-cats/our-cats.component.ts
+++ app-frontend/src/app/our-cats/our-cats.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { CommonModule } from '@angular/common';
 import { CatComponent } from '../cat/cat.component';
-import { CatsService } from '../../services/cat.service';
-import { Cat } from '../../interfaces/cat';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-cats',
@@ -11,7 +10,8 @@ import { Cat } from '../../interfaces/cat';
   imports: [
     NavigationComponent,
     CommonModule,
-    CatComponent
+    CatComponent,
+    RouterLink
   ],
   templateUrl: './our-cats.component.html',
   styleUrl: './our-cats.component.css'
