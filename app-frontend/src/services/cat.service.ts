b32bc32f7eea5330f0645471050e5704d987e4fc
Vanessa <vanessa.munson03@gmail.com>
2024-06-19 06:35:29 +0200

    Cat service with functions to get, add, and delete cats

--- app-frontend/src/services/cat.service.ts
+++ app-frontend/src/services/cat.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Cat } from '../interfaces/cat';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class CatService {
+export class CatsService {
+  private apiUrl = 'http://127.0.0.1:8000/api/cats';
 
+  constructor(private http: HttpClient) { }
+
+  getCats(): Observable<Cat[]> {
+    return this.http.get<Cat[]>(this.apiUrl);
+  }
+
+  addCat(cat: FormData): Observable<Cat> {
+    return this.http.post<Cat>(this.apiUrl, cat);
+  }
+
+  deleteCat(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
