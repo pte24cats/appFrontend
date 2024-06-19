f3ade75b591e5bca3f74487dcc8c26e0719fe2e5
Vanessa <vanessa.munson03@gmail.com>
2024-06-19 06:34:51 +0200

    Cat interface for data

--- app-frontend/src/interfaces/cat.ts
+++ app-frontend/src/interfaces/cat.ts
@@ -1,3 +1,11 @@
 export interface Cat {
-    
-}
+    id?: number;
+    cat_name: string;
+    cat_color: string;
+    cat_description: string;
+    good_cat_status: boolean;
+    cat_age: number;
+    cat_gender: string;
+    cat_breed: string;
+    cat_picture?: File | string | null;
+  }
